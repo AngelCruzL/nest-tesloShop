4a0512d70a3f3bdb0f50493880be6de150054e81
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-10-22 23:56:55 -0500

    feat: add tags column

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -38,4 +38,9 @@ export class CreateProductDto {
 
   @IsIn(['men', 'women', 'kid', 'unisex'])
   gender: string;
+
+  @IsString({ each: true })
+  @IsArray()
+  @IsOptional()
+  tags?: string[];
 }
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -32,6 +32,9 @@ export class Product {
   @Column('text')
   gender: string;
 
+  @Column('text', { array: true, default: [] })
+  tags: string[];
+
   @BeforeInsert()
   checkSlugInsert() {
     if (!this.slug) this.slug = this.title;
