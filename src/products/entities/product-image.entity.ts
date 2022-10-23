63ba535373dd16865da9f78200fda216e63c665a
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-10-23 15:36:35 -0500

    feat: add one to many and many to one relationships

--- src/products/entities/product-image.entity.ts
+++ src/products/entities/product-image.entity.ts
@@ -1,4 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+import { Product } from './product.entity';
 
 @Entity()
 export class ProductImage {
@@ -7,4 +9,7 @@ export class ProductImage {
 
   @Column('text')
   url: string;
+
+  @ManyToOne(() => Product, (product) => product.images)
+  product: Product;
 }
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -3,8 +3,11 @@ import {
   BeforeUpdate,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+
+import { ProductImage } from './product-image.entity';
 
 @Entity()
 export class Product {
@@ -34,6 +37,11 @@ export class Product {
 
   @Column('text', { array: true, default: [] })
   tags: string[];
+
+  @OneToMany(() => ProductImage, (productImage) => productImage.product, {
+    cascade: true,
+  })
+  images?: ProductImage[];
 
   @BeforeInsert()
   checkSlugInsert() {
