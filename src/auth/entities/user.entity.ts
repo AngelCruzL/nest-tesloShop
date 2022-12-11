1c9a23bb5f2518ebeac4a736491656bb9bfad2d0
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-12-11 10:05:40 -0600

    feat: add user entity

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', { unique: true })
+  email: string;
+
+  @Column('text')
+  password: string;
+
+  @Column('text')
+  fullName: string;
+
+  @Column('bool', { unique: true })
+  isActive: boolean;
+
+  @Column('text', { array: true, default: ['user'] })
+  roles: string[];
+}
