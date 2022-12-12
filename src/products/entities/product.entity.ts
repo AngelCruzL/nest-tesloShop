5971e928e4dd1a98720affdb98bb604d2ff968b7
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-12-12 14:14:11 -0600

    docs: add product entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   BeforeInsert,
   BeforeUpdate,
@@ -13,33 +14,80 @@ import { ProductImage } from './product-image.entity';
 
 @Entity({ name: 'products' })
 export class Product {
+  @ApiProperty({
+    example: '1c0579ee-2ff4-45ed-9201-37e3f1839fff',
+    description: 'Product ID',
+    uniqueItems: true,
+  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty({
+    example: 'T-Shirt Teslo',
+    description: 'Product Title',
+    uniqueItems: true,
+  })
   @Column('text', { unique: true })
   title: string;
 
+  @ApiProperty({
+    example: 0,
+    description: 'Product price',
+  })
   @Column('float', { default: 0 })
   price: number;
 
+  @ApiProperty({
+    example:
+      'Inspired by our popular home battery, the Tesla Powerwall Tee is made from 100% cotton and features the phrase " Pure Energy"',
+    description: 'Product description',
+    default: null,
+  })
   @Column({ type: 'text', nullable: true })
   description: string;
 
+  @ApiProperty({
+    example: 't-shirt-teslo',
+    description: 'Product slug - for SEO',
+    uniqueItems: true,
+  })
   @Column('text', { unique: true })
   slug: string;
 
+  @ApiProperty({
+    example: 10,
+    description: 'Product stock',
+    default: 0,
+  })
   @Column('int', { default: 0 })
   stock: number;
 
+  @ApiProperty({
+    example: ['M', 'XL', 'XXL'],
+    description: 'Product sizes',
+  })
   @Column('text', { array: true })
   sizes: string[];
 
+  @ApiProperty({
+    example: 'women',
+    description: 'Product gender',
+  })
   @Column('text')
   gender: string;
 
+  @ApiProperty({
+    example: ['shirt'],
+    description: 'Product tags',
+  })
   @Column('text', { array: true, default: [] })
   tags: string[];
 
+  @ApiProperty({
+    example: ['9877040-00-A_0_2000.jpg', '9877040-00-A_1.jpg'],
+    description: 'Product Images',
+    uniqueItems: true,
+  })
   @OneToMany(() => ProductImage, (productImage) => productImage.product, {
     cascade: true,
     eager: true,
