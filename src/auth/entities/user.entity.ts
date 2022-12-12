52edc9a327c054ccb34f5cd5726ddfec465ac52f
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-12-12 10:44:51 -0600

    feat: add product-user relation

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -3,8 +3,11 @@ import {
   BeforeUpdate,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+
+import { Product } from './../../products/entities/product.entity';
 
 @Entity()
 export class User {
@@ -25,6 +28,9 @@ export class User {
 
   @Column('text', { array: true, default: ['user'] })
   roles: string[];
+
+  @OneToMany(() => Product, (product) => product.user)
+  product: Product;
 
   @BeforeInsert()
   checkFieldsBeforeInsert() {
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -3,10 +3,12 @@ import {
   BeforeUpdate,
   Column,
   Entity,
+  ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
+import { User } from './../../auth/entities/user.entity';
 import { ProductImage } from './product-image.entity';
 
 @Entity({ name: 'products' })
@@ -43,6 +45,9 @@ export class Product {
     eager: true,
   })
   images?: ProductImage[];
+
+  @ManyToOne(() => User, (user) => user.product, { eager: true })
+  user: User;
 
   @BeforeInsert()
   checkSlugInsert() {
