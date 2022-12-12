86f6d659facb9b63faba6484059cbfb57fc1ca25
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-12-11 19:22:56 -0600

    feat: add email data to get user decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -22,11 +22,15 @@ export class AuthController {
 
   @Get('private')
   @UseGuards(AuthGuard())
-  testingPrivateRoute(@GetUser() user: User) {
+  testingPrivateRoute(
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+  ) {
     return {
       ok: true,
       message: 'This route is private',
       user,
+      userEmail,
     };
   }
 }
--- src/auth/decorators/get-user.decorator.ts
+++ src/auth/decorators/get-user.decorator.ts
@@ -4,11 +4,14 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 
-export const GetUser = createParamDecorator((data, ctx: ExecutionContext) => {
-  const req = ctx.switchToHttp().getRequest();
-  const user = req.user;
+export const GetUser = createParamDecorator(
+  (data: string, ctx: ExecutionContext) => {
+    const req = ctx.switchToHttp().getRequest();
+    const user = req.user;
 
-  if (!user) throw new InternalServerErrorException('User not found (request)');
+    if (!user)
+      throw new InternalServerErrorException('User not found (request)');
 
-  return user;
-});
+    return !data ? user : user[data];
+  },
+);
