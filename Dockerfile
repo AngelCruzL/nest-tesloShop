9028645da456532e14e0dd3927fffd3be14da866
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-12-31 17:22:45 -0600

    feat: add docker to nest project

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# syntax=docker/dockerfile:1
+
+FROM node:18-alpine AS base
+
+WORKDIR /app
+
+COPY [ "package.json", "yarn.lock*", "./" ]
+
+FROM base AS dev
+ENV NODE_ENV=development
+RUN yarn install --frozen-lockfile
+COPY . .
+CMD [ "yarn", "start:dev" ]
+
+FROM base AS prod
+ENV NODE_ENV=production
+RUN yarn install --frozen-lockfile --production
+COPY . .
+RUN yarn global add @nestjs/cli
+RUN yarn build
+CMD [ "yarn", "start:prod" ]
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,22 +4,22 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 
+import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
 import { FilesModule } from './files/files.module';
 import { ProductsModule } from './products/products.module';
 import { SeedModule } from './seed/seed.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
+      host: 'db',
       port: +process.env.DB_PORT,
       database: process.env.DB_NAME,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
       autoLoadEntities: true,
       synchronize: true,
     }),
