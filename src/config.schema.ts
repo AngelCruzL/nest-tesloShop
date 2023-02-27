2cf5edbed2ebaefe629631ee237efaeab1ec61bb
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-02-27 10:50:45 -0600

    fix: joi port validation

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -1,8 +1,9 @@
 import * as Joi from 'joi';
+import * as process from 'process';
 
 export const configValidationSchema = Joi.object({
   STAGE: Joi.string().required(),
-  PORT: Joi.number().default(3000).required(),
+  PORT: Joi.number() || process.env.PORT,
   DB_HOST: Joi.string().required(),
   DB_PORT: Joi.number().default(5432).required(),
   DB_USER: Joi.string().required(),
